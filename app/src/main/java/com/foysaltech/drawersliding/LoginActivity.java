b6bf2e07832fa50176112bbba1b7a35cb2bde7bd
NegroLoco12 <106832898+NegroLoco12@users.noreply.github.com>
2023-02-04 10:40:03 -0300

    actualizacion

--- app/src/main/java/com/foysaltech/drawersliding/LoginActivity.java
+++ app/src/main/java/com/foysaltech/drawersliding/LoginActivity.java
@@ -140,7 +140,7 @@ public class LoginActivity extends AppCompatActivity {
 
         ////////////////////////////////////////////////////////////////////////////////////////////
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.default_web_client_ide))
                 .requestEmail()
                 .build();
 
