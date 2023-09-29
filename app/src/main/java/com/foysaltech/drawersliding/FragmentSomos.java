3151b526116fb1ea50fdc526954037955aa5ef76
USUARIO <gh1smati@gmail.cpm>
2023-09-29 17:34:35 -0400

    nuevo

--- app/src/main/java/com/foysaltech/drawersliding/FragmentSomos.java
+++ app/src/main/java/com/foysaltech/drawersliding/FragmentSomos.java
@@ -1,5 +1,7 @@
 package com.foysaltech.drawersliding;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +9,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class FragmentSomos extends Fragment {
 
-
+    ImageView face,instagram;
     public FragmentSomos() {
 
     }
@@ -31,7 +35,33 @@ public class FragmentSomos extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_somos, container, false);
+      View view =inflater.inflate(R.layout.fragment_somos, container, false);
+
+    face=view.findViewById(R.id.face);
+        instagram=view.findViewById(R.id.instagram);
+        face.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("https://www.instagram.com/prodiser_saic/?hl=es");
+                startActivity(new Intent(Intent.ACTION_VIEW,uri));
+            }
+        });
+        face=view.findViewById(R.id.face);
+
+        face.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("https://www.facebook.com/ProdiserSaic/photos?locale=es_LA");
+                startActivity(new Intent(Intent.ACTION_VIEW,uri));
+            }
+        });
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("https://www.instagram.com/prodiser_saic/?hl=es");
+                startActivity(new Intent(Intent.ACTION_VIEW,uri));
+            }
+        });
+        return view;
     }
 }
